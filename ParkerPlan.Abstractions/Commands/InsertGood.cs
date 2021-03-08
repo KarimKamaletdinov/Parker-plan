263e4d7b5ab3809351c8a9ff7aefbf01986c4862
karim <karimkamaletdinov55@gmail.com>
2021-03-08 12:53:18 +0500

    +InsertGood

---
+++ ParkerPlan.Abstractions/Commands/InsertGood.cs
@@ -0,0 +1,10 @@
+﻿using System.Dynamic;
+using ParkerPlan.Abstractions.Dtos;
+
+namespace ParkerPlan.Abstractions.Commands
+{
+    public class InsertGood : Command
+    {
+        public GoodDto Good { get; set; }
+    }
+}
