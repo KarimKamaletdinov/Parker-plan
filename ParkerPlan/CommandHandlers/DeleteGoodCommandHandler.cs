4dfce59b5a4cc4346d940df2a62a8bd72fabe401
karim <karimkamaletdinov55@gmail.com>
2021-03-08 13:25:42 +0500

    +DeleteGoodCommandHandler

---
+++ ParkerPlan/CommandHandlers/DeleteGoodCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using ParkerPlan.Abstractions;
+using ParkerPlan.Abstractions.Commands;
+using ParkerPlan.Repositories;
+
+namespace ParkerPlan.CommandHandlers
+{
+    public class DeleteGoodCommandHandler : ICommandHandler<DeleteGood>
+    {
+        private SqlGoodRepository _repository;
+
+        public DeleteGoodCommandHandler(SqlGoodRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(DeleteGood command)
+        {
+            _repository.Delete(command.GoodId);
+        }
+    }
+}
