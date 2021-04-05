fe492bbd37368e3b153f334e13027292ab8d8ef5
karim <karimkamaletdinov55@gmail.com>
2021-04-05 18:03:23 +0500

    Some additions in WorkerDto

--- ParkerPlan.Abstractions/Dtos/WorkerDto.cs
+++ ParkerPlan.Abstractions/Dtos/WorkerDto.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Patronymic { get; set; }
+        public string About { get; set; }
+        public string Position { get; set; }
         public string Password { get; set; }
         public int[] MyLeadIds { get; set; }
     }
